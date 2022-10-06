4fd733b12dcdd9c5869ff1c051030527633d357c
prajwalsuvarna <prajwalsuvarna1906@gmail.com>
2022-10-06 22:40:53 +0530

    postgres Database configuration and created users table

--- pharmacy/members/models.py
+++ pharmacy/members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id=models.IntegerField(primary_key=True)
+    entity_id=models.IntegerField()
+    user_name=models.CharField(max_length=100)
+    emp_id=models.IntegerField()
+    user_password=models.CharField(max_length=200)
+    is_active=models.BooleanField(default=True)
+    
+
--- pharmacy/pharmacy/settings.py
+++ pharmacy/pharmacy/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'pharmacy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'pharmacy',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost'
     }
 }
 
