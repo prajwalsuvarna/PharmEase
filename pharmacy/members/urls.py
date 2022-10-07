9213755caa4cec773a601b75120fcb461414c269
prajwalsuvarna <prajwalsuvarna1906@gmail.com>
2022-10-07 19:22:36 +0530

    created Registration and Home page ui

--- pharmacy/members/urls.py
+++ pharmacy/members/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('',views.index,name="home"),
     path('login/',views.loginUser,name="login"),
     path('logout/',views.logoutUser,name="logout"),
-   
+    path('register/',views.register,name="register"),
    
 ]
--- pharmacy/members/views.py
+++ pharmacy/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login,logout
+from django.contrib import messages
 # Create your views here.
 def index(request):
     if request.user.is_anonymous:
@@ -24,3 +25,30 @@ def loginUser(request):
 def logoutUser(request):
     logout(request)
     return redirect("/login")
+
+def register(request):
+    if request.method == 'POST':
+       first_name=request.POST['first_name']
+       last_name=request.POST['last_name']
+       username=request.POST['username']
+       email=request.POST['email']
+       password1=request.POST['password1']
+       password2=request.POST['password2']
+       
+       if password1==password2:
+           if User.objects.filter(username=username).exists():
+               messages.info(request,'Username taken')
+               return redirect('/register')
+           elif User.objects.filter(email=email).exists():
+               messages.info(request,'Email taken')
+               return redirect('/register')
+           else:
+               user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+               user.save()
+               print('User Created')
+       else:
+           messages.info(request,'Password not matching')
+           return redirect('/register')
+       return redirect('/login')
+    else:
+       return render(request,'register.html')
