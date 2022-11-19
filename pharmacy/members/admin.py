7b510596a2736e448462ca0dfce3f4ad03a7f3f6
prajwalsuvarna <prajwalsuvarna1906@gmail.com>
2022-11-19 23:44:35 +0530

    Admin model registration:DrgModel,DistModel

--- pharmacy/members/admin.py
+++ pharmacy/members/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import EmpModel
+from .models import EmpModel,DrgModel,DistModel
 # Register your models here.
 admin.site.register(EmpModel)
+admin.site.register(DrgModel)
+admin.site.register(DistModel)
