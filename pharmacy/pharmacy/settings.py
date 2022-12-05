039a44cacd57ef82add4f102330e3b2e1bc39331
prajwalsuvarna <prajwalsuvarna1906@gmail.com>
2022-12-05 17:30:40 +0530

    Removed multiforpackage folder

--- pharmacy/pharmacy/settings.py
+++ pharmacy/pharmacy/settings.py
@@ -65,9 +65,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'libraries':{
-            'multifor': 'members.templatetags.multifor',
-            }
         },
 
     },
